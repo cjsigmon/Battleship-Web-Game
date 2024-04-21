4ee94d59f6ccec3cbdc3f8a73c78328a70cc4b13
Caleb Sigmon <cjsigmon@email.unc.edu>
2024-04-20 23:47:09 -0400

    express-session

--- battleship/index.js
+++ battleship/index.js
@@ -1,18 +1,20 @@
 const express = require('express')
+var session = require('express-session');
+
 const app = express()
 const port = 3000
 const path = require('path')
 
 
-app.use(express.static(path.join(__dirname, 'frontend')));
+// app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'keyboard cat'
+  }));
 
 
 
-// app.get('/', (req, res) => {
-//   res.send("hello");
-//   app.use(express.static(path.join(__dirname, 'frontend')));
-
-// })
 
 app.post('/', (req, res) => {
     res.send('Got a POST request')
