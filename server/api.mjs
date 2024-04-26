7cd0eeab8cfbef765b72c0995da2c2be7e843ae6
Caleb Sigmon <cjsigmon@email.unc.edu>
2024-04-25 20:21:39 -0400

    endpoint for posting new score

--- server/api.mjs
+++ server/api.mjs
@@ -1,15 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { db } from './db.mjs';
 
 export function runAPI() {
     const app = express();
     const port = 7777;
     app.use(bodyParser.json());
 
+    app.get('/scores', async (req, res) => {
+        const rows = await db.all(`
+            SELECT * FROM scores
+            ORDER BY score DESC
+        `);
+        res.json(rows);
+    });
+
+    app.post('/scores', async (req, res) => {
+
+        
+        await db.run(`
+            INSERT INTO scores (initials, score) VALUES
+            ('${(req.body.initials)}', ${(req.body.score)})
+        `);
+        res.json(req.body)
+
+    })
+
     app.get('/', (req, res) => {
         // Replace with your code
         res.json('HELLO THERE LITTLE ONE');
     });
+
+
     
     
     app.listen(port, () => {
--- server/setup_db.mjs
+++ server/setup_db.mjs
@@ -6,33 +6,14 @@ export async function setupDB() {
 
 // Use the db object to run table creation commands and otherwise initialize your database setup here.
 await db.run(`
-CREATE TABLE IF NOT EXISTS scores (
-    id INTEGER PRIMARY KEY,
-    initials TEXT(3) NOT NULL,
-    score INTEGER
-);
+        CREATE TABLE IF NOT EXISTS scores (
+        id INTEGER PRIMARY KEY,
+        initials TEXT(3) NOT NULL,
+        score INTEGER
+        );
+        `);
 
-`);
 
-await db.run(`
-        INSERT INTO scores (initials, score) VALUES
-        ('CJS', 100)
-`);
-
-await db.run(`
-        INSERT INTO scores (initials, score) VALUES
-        ('HER', 10)
-`);
-
-await db.run(`
-        INSERT INTO scores (initials, score) VALUES
-        ('ROT', 100)
-`);
-
-await db.run(`
-        INSERT INTO scores (initials, score) VALUES
-        ('HIM', 10)
-`);
 
 const rows = await db.all(`
         SELECT * FROM scores
